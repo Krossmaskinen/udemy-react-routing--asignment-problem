1146267e06bb9eb6ee2e35c2e67c5e4a9001028a
Bill Kurtson <bill.kurtson@gmail.com>
2018-10-10 10:05:36 +0200

    Course: - make courses clickable - display id in course route

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 
 import Courses from './containers/Courses/Courses';
 import Users from './containers/Users/Users';
+import Course from './containers/Course/Course';
 
 class App extends Component {
     render() {
@@ -38,6 +39,7 @@ class App extends Component {
                         <Switch>
                             <Route path="/users" component={Users} />
                             <Route path="/courses" component={Courses} />
+                            <Route path="/course/:id" component={Course} />
                         </Switch>
                     </main>
                 </div>
